6624ca13edaad4c6ef79153de862019d08c7b06c
tjsdla520 <83327352+tjsdla520@users.noreply.github.com>
2021-07-29 16:59:27 +0900

    7/29 스프링부트 기본강의 내용정리
    1. JPA
    2. 스프링 데이터 JPA
    3. 스프링 데이터 JPA 제공기능

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
-public class Member {
+import javax.persistence.*;
 
+@Entity
+public class Member {
+    //db가 알아서 생성해주는것을 identity
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 데이터를 구별하기위한 시스템이 저장하는 임의값
+
     private String name; //이름
 
     public Long getId() {
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     //DI = dependency injection
     //외부에서 따로 생성하지않고 직접 넎어준다.
